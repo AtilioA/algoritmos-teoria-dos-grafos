44e677d3573fccb866ff87d3c9ca7580c2875bad
AtilioA <atiliodadalto@hotmail.com>
2019-10-19 20:13:42 -0300

    Ajustes no script de BFS

--- grafo_bfs.py
+++ grafo_bfs.py
@@ -3,9 +3,13 @@ from collections import defaultdict, deque
 
 # Grafo direcionado com lista de adjacências
 class Graph:
-    def __init__(self):
+    def __init__(self, nVertices):
         # default dictionary para armazenar o grafo
         self.graph = defaultdict(list)
+        self.nVertices = nVertices
+        # Inicializa listas de adjacências dos vértices
+        for i in range(nVertices):
+            self.graph[i] = []
 
     def adiciona_aresta(self, u, v):
         self.graph[u].append(v)
@@ -19,19 +23,19 @@ class Graph:
         # (deque = Double Ended Queue)
         queue = deque()
 
-        # Marca o vértice de entrada como marcado e enfilera-o
+        # Marca o vértice de entrada e enfileira-o
         queue.append(inicio)
         visited[inicio] = True
 
         # Enquanto a fila tiver vértices
         while queue:
             print(f"Fila atual: {queue}")
-            # Desenfilera um vértice e printa
+            # Desenfileira um vértice e printa
             desenfileirado = queue.popleft()
             print(f"Vértice visitado: {desenfileirado}")
 
             # Pega os adjacentes ao vértice desenfileirado
-            # Se não estiver visitado, marca-o e enfilera-o
+            # Se não estiver visitado, marca-o e enfileira-o
             # Nova iteração com a nova fila até que terminem os vizinhos
             for adj in self.graph[desenfileirado]:
                 print(f"Adjacentes a {desenfileirado}:")
@@ -45,18 +49,38 @@ class Graph:
                     print(f"{adj} já foi marcado.")
             print()
 
+    def BFS_print(self, inicio):
+        visited = [False] * (len(self.graph) + 1)
+        visited[inicio] = True
 
+        queue = deque()
+        queue.append(inicio)
+
+        while queue:
+            desenfileirado = queue.popleft()
+            print(f"{desenfileirado:3}\n")
+
+            for adj in self.graph[desenfileirado]:
+                if visited[adj] is False:
+                    visited[adj] = True
+                    queue.append(adj)
+
+
+g = Graph(9)
 # Cria um grafo e adiciona arestas
-g = Graph()
-
 g.adiciona_aresta(0, 1)
-g.adiciona_aresta(1, 2)
-g.adiciona_aresta(1, 3)
+g.adiciona_aresta(0, 2)
 g.adiciona_aresta(2, 3)
-g.adiciona_aresta(2, 0)
+g.adiciona_aresta(2, 4)
+g.adiciona_aresta(3, 5)
+g.adiciona_aresta(3, 6)
+g.adiciona_aresta(4, 7)
+g.adiciona_aresta(4, 8)
 
 print(f"\nGrafo: {g.graph.items()}")
 
 s = 0
-print(f"\nRealizando busca em amplitude (breadth-first traversal) começando do vértice {s}:\n")
+print(f"\nRealizando busca em amplitude (breadth-first traversal)\
+      começando do vértice {s}:\n")
 g.BFS(s)
+g.BFS_print(s)
