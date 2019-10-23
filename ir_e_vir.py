f2d10e4b016ab7d9be11440baaea0957cf892096
AtilioA <atiliodadalto@hotmail.com>
2019-10-23 17:39:33 -0300

    Accepted

--- ir_e_vir.py
+++ ir_e_vir.py
@@ -7,6 +7,8 @@ class Grafo:
         # default dictionary para armazenar o grafo
         self.grafo = defaultdict(list)
         self.nVertices = nVertices
+        self.visif.grafo = defaultdict(list)
+        self.nVertices = nVertices
         self.visitado = [False] * (self.nVertices)
         # Inicializa listas de adjacências dos vértices
         for i in range(nVertices):
@@ -18,8 +20,18 @@ class Grafo:
 
     def busca(self, v):
         # Marca o vértice de entrada como visitado
+        self.visitado[v] = Truetado = [False] * (self.nVertices)
+        # Inicializa listas de adjacências dos vértices
+        for i in range(nVertices):
+            self.grafo[i] = []
+
+    def adiciona_aresta(self, u, v):
+        # O vértice u possui aresta incidindo exteriormente para v
+        self.grafo[u].append(v)
+
+    def busca(self, v):
+        # Marca o vértice de entrada como visitado
         self.visitado[v] = True
-        # print(v, end=' ')
 
         # Recursão com os vértices adjacentes ao de entrada
         for i in self.grafo[v]:
@@ -32,8 +44,6 @@ class Grafo:
 
         # Recursão com os vértices adjacentes ao de entrada
         for i in self.grafo[v]:
-            # print(i)
-            # print(self.visitado)
             if self.visitado[i] is False:
                 self.percorre_adiciona_pilha(i, pilhaDeVertices)
 
@@ -59,18 +69,16 @@ class Grafo:
             if self.visitado[i] is False:
                 self.percorre_adiciona_pilha(i, pilhaDeVertices)
 
-        # Cria um grafo reverso
+        # Cria o grafo reverso
         grafoReverso = self.cria_grafo_reverso()
 
         nComponentesFConexas = 0
-        # Printar vértices do da pilha de busca do grafo reverso
         while pilhaDeVertices:
             i = pilhaDeVertices.pop()
             if grafoReverso.visitado[i] is False:
-                # print(f"Componente nº {nComponentesFConexas}:")
                 grafoReverso.busca(i)
-                # print("\n")
                 nComponentesFConexas += 1
+
         return nComponentesFConexas
 
 
@@ -79,7 +87,6 @@ if __name__ == "__main__":
         entrada = input().strip().split(' ')
         N = int(entrada[0])
         M = int(entrada[1])
-        # print(f"N = {N}, M = {M}")
         if N == 0 and M == 0:
             break
 
@@ -87,10 +94,10 @@ if __name__ == "__main__":
 
         for vertice in range(M):
             entradaArestas = input().strip().split(' ')
+            # Subtraindo 1 pois vértices do grafo começam do 0
             V = int(entradaArestas[0]) - 1
             W = int(entradaArestas[1]) - 1
             P = int(entradaArestas[2])
-            # print(f"V = {V}, W = {W}, P = {P}\n")
 
             if P == 1:
                 g.adiciona_aresta(V, W)
@@ -99,7 +106,6 @@ if __name__ == "__main__":
                 g.adiciona_aresta(W, V)
 
         nComponentesFConexas = g.componentes_f_conexas()
-        # print(f"Número de componentes f-conexas: {nComponentesFConexas}\n\n")
         if nComponentesFConexas != 1:
             print(0)
         else:
