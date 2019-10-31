b73f832781a0967452208f7503e2aa7ec1776df8
AtilioA <atiliodadalto@hotmail.com>
2019-10-31 11:15:45 -0300

    Testando dicionário para lista de adjacências

--- Java/Main.java
+++ Java/Main.java
@@ -1,35 +1,8 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 // import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Vertex v1;
-        v1 = new Vertex(1);
-        Vertex v2;
-        v2 = new Vertex(2);
-        Vertex v3;
-        v3 = new Vertex(3);
-        ArrayList<Vertex> vertices = new ArrayList<Vertex>(3);
-        vertices.add(v1);
-        vertices.add(v2);
-        vertices.add(v3);
-
-        System.out.println("Vertices infos:\n");
-        for (int i = 0; i < vertices.size(); i++) {
-            System.out.println(vertices.get(i).getID() + ", connected to " + vertices.get(i).getAdjacency());
-        }
-
-        System.out.println("\nCreating undirected graph and adding connections:");
-        Graph graph;
-        graph = new Graph(vertices);
-        graph.addConnection(1, 3);
-        graph.addConnection(2, 3);
-
-        for (int i = 0; i < graph.getVertices().size(); i++) {
-            System.out.println(graph.getVertices().get(i).getID() + ", connected to " + graph.getVertices().get(i).getAdjacency());
-        }
-    }
-}
 
 class Vertex {
     int id;
@@ -134,5 +107,43 @@ class Graph {
     public void addConnection(int u, int v){
         this.vertices.get(u - 1).adjacency.add(v);
         this.vertices.get(v - 1).adjacency.add(u);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Testing HashMap to store adjacency vertices
+        Map<Integer, List<Integer>> dictionary = new HashMap<Integer, List<Integer>>();
+        dictionary.computeIfAbsent(10, k -> new ArrayList<>()).add(5);
+        dictionary.computeIfAbsent(10, k -> new ArrayList<>()).add(3);
+        dictionary.computeIfAbsent(10, k -> new ArrayList<>()).add(2);
+        dictionary.computeIfAbsent(10, k -> new ArrayList<>()).add(1);
+        System.out.println("Vertices adjacent to vertex 10: " + dictionary.get(10));
+
+        Vertex v1;
+        v1 = new Vertex(1);
+        Vertex v2;
+        v2 = new Vertex(2);
+        Vertex v3;
+        v3 = new Vertex(3);
+        ArrayList<Vertex> vertices = new ArrayList<Vertex>(3);
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+
+        System.out.println("\nVertices infos:\n");
+        for (int i = 0; i < vertices.size(); i++) {
+            System.out.println(vertices.get(i).getID() + ", connected to " + vertices.get(i).getAdjacency());
+        }
+
+        System.out.println("\nCreating undirected graph and adding connections:");
+        Graph graph;
+        graph = new Graph(vertices);
+        graph.addConnection(1, 3);
+        graph.addConnection(2, 3);
+
+        for (int i = 0; i < graph.getVertices().size(); i++) {
+            System.out.println(graph.getVertices().get(i).getID() + ", connected to " + graph.getVertices().get(i).getAdjacency());
+        }
     }
 }
