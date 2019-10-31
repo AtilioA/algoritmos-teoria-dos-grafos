044cbe26100262a953a16cbc8413f5fd55d2da10
AtilioA <atiliodadalto@hotmail.com>
2019-10-30 23:25:07 -0300

    Testando implementação de grafo em Java

---
+++ Java/Main.java
@@ -0,0 +1,138 @@
+import java.util.ArrayList;
+// import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Vertex v1;
+        v1 = new Vertex(1);
+        Vertex v2;
+        v2 = new Vertex(2);
+        Vertex v3;
+        v3 = new Vertex(3);
+        ArrayList<Vertex> vertices = new ArrayList<Vertex>(3);
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+
+        System.out.println("Vertices infos:\n");
+        for (int i = 0; i < vertices.size(); i++) {
+            System.out.println(vertices.get(i).getID() + ", connected to " + vertices.get(i).getAdjacency());
+        }
+
+        System.out.println("\nCreating undirected graph and adding connections:");
+        Graph graph;
+        graph = new Graph(vertices);
+        graph.addConnection(1, 3);
+        graph.addConnection(2, 3);
+
+        for (int i = 0; i < graph.getVertices().size(); i++) {
+            System.out.println(graph.getVertices().get(i).getID() + ", connected to " + graph.getVertices().get(i).getAdjacency());
+        }
+    }
+}
+
+class Vertex {
+    int id;
+    int distance;
+    boolean visited;
+    ArrayList<Integer> adjacency;
+
+    public Vertex(int id, ArrayList<Integer> adjacency) {
+        this.distance = Integer.MAX_VALUE; // "Infinite" value to initialize distances
+        this.id = id;
+        this.adjacency = adjacency;
+        this.visited = false;
+    }
+
+    public Vertex(int id) {
+        this.distance = Integer.MAX_VALUE;
+        this.id = id;
+        this.adjacency = new ArrayList<Integer>();
+        this.visited = false;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public void setDistance(int dist) {
+        this.distance = dist;
+    }
+
+    public ArrayList<Integer> getAdjacency() {
+        return adjacency;
+    }
+
+    public void setAdjacency(ArrayList<Integer> adjacency) {
+        this.adjacency = adjacency;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+}
+
+class Edge {
+    int originVertex, destinationVertex;
+
+    public Edge(int originVertex, int destinationVertex) {
+        super();
+        this.originVertex = originVertex;
+        this.destinationVertex = destinationVertex;
+    }
+
+    public int getOriginVertex() {
+        return originVertex;
+    }
+
+    public void setOriginVertex(int originVertex) {
+        this.originVertex = originVertex;
+    }
+
+    public int getDestinationVertex() {
+        return destinationVertex;
+    }
+
+    public void setDestinationVertex(int destinationVertex) {
+        this.destinationVertex = destinationVertex;
+    }
+
+}
+
+class Graph {
+    ArrayList<Vertex> vertices;
+
+    public Graph() {
+        this.vertices = new ArrayList<Vertex>();
+    }
+
+    public Graph(ArrayList<Vertex> vertices) {
+        super();
+        this.vertices = vertices;
+    }
+
+    public ArrayList<Vertex> getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(ArrayList<Vertex> vertices) {
+        this.vertices = vertices;
+    }
+
+    public void addConnection(int u, int v){
+        this.vertices.get(u - 1).adjacency.add(v);
+        this.vertices.get(v - 1).adjacency.add(u);
+    }
+}
