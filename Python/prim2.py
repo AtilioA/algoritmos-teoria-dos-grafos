0eb137cde7dbba3d78cb3f71f8c37686dea14f8e
AtilioA <atiliodadalto@hotmail.com>
2019-11-27 22:29:05 -0300

    Pequenos ajustes no Prim

--- Python/prim2.py
+++ Python/prim2.py
@@ -5,12 +5,9 @@ class Grafo:
         self.adjs = defaultdict(list)
         self.nVertices = nVertices
         self.peso = 0
-        # Inicializa todos os vértices
+        # Inicializa todos os vértices acessando o defaultdict
         for vertice in range(nVertices):
-            self.adjs[vertice] = []
-        # <<Conjuntos>> para Dijkstra
-        self.fechado = [False] * nVertices
-        self.aberto = [True] * nVertices
+            self.adjs[vertice]
 
     def adiciona_aresta(self, u, v, peso):
         self.peso += peso
@@ -27,23 +24,28 @@ class Grafo:
 
     def prim(self):
         T = Grafo(self.nVertices)
-        Vl = set({0})
+        Vl = {0}
         L = [float("inf")] * self.nVertices
         V = set(range(self.nVertices))
+
         for v in self.adjs[0]:
             L[v[0]] = v[1]
-        while bool(V - Vl):
+
+        while V - Vl:
             w = L.index(min([L[x] for x in (V - Vl)]))
-            wAdjs = [x for x in self.adjs[w] if x[0] in Vl] 
+            wAdjs = [x for x in self.adjs[w] if x[0] in Vl]
+
             pesos = list(map(lambda x: x[1], wAdjs))
             menorPeso = min(pesos)
             indiceMenor = pesos.index(menorPeso)
             u = wAdjs[indiceMenor][0]
+
             T.adiciona_aresta(u, w , self.getPeso(u, w))
             Vl = Vl | set({w})
             for v in (V - Vl):
                 if self.getPeso(v, w) < L[v]:
                     L[v] = self.getPeso(v, w)
+
         return T
 
 
@@ -55,7 +57,7 @@ if __name__ == "__main__":
     G.adiciona_aresta(1, 2, 5)
     G.adiciona_aresta(1, 3, 0)
     G.adiciona_aresta(2, 3, 4)
-    print(G.adjs.items())
+    print(f"Grafo G:\n{dict(G.adjs.items())}")
+
     T = G.prim()
-    print(T.adjs.items())
-    print(T.peso)
+    print(f"Árvore geradora mínima de G, peso {T.peso}:\n{dict(T.adjs.items())}")
